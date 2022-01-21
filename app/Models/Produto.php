80fd0d4f02c2692b7a7097bd67cb801afd19960b
Caca404 <christia10k@hotmail.com>
2022-01-20 23:30:11 -0300

    Alterações basicas no código e css

---
+++ app/Models/Produto.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+
+class Produto
+{
+    public $id;
+    public $nome;
+    public $descricao;
+    public $data_criacao;
+    public $quant_estoque;
+    public $preco;
+    public $tipo;
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,14 +17,14 @@
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
-                <div class="card col-12 col-md-5 col-lg-3 mt-4 mt-lg-0">
+                <div class="card col-12 col-md-5 col-lg-3 mt-4 mt-md-0">
                     <img src="/img/Camisa2.png" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
-                <div class="card col-12 col-md-5 col-lg-3 mt-4 mt-lg-0">
+                <div class="card col-12 col-md-5 col-lg-3 mt-4 mt-md-0">
                     <img src="/img/Camisa3.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
